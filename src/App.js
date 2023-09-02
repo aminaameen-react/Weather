df525c9cdd56d3bdc3a7a7d482e6a2de90a3a445
Amina Ameen <aminaameen147@gmail.com>
2023-09-02 17:47:21 +0500

    added changes

--- src/App.js
+++ src/App.js
@@ -3,22 +3,60 @@ import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [loading,setLoading]=useState(false);
   const [list, setList] = useState([]);
   const [city,setCity]=useState('');
+  
+  function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function checkCookie() {
+  let user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+     user = prompt("Please enter your name:","");
+     if (user != "" && user != null) {
+       setCookie("username", user, 30);
+     }
+  }
+}
     
   const fun = event => {
-    setCity(event.target.value);
+     setCity(event.target.value);
+     setLoading(true);
     
     axios.get(`https://api.weatherapi.com/v1/forecast.json?q=${city}&days=7&key=08dd6b64a84741fc8cb70125232408`)
     .then(response => {
+      setLoading(false);
       setList(response.data.forecast.forecastday);
       console.log(response.data);
     })
     .catch(error => {
       console.error(error);
+      setLoading(false);
     });  
   }
   return (
+    !loading?
     <div className="App">
           <div className="input">
 
@@ -31,6 +69,17 @@ function App() {
       onChange={fun}
       />
                </div>
+              
+               <div className="input">
+
+<input
+type='text'
+className='textInput'
+placeholder='Enter Name'
+maxLength={50}
+onChange={checkCookie}
+/>
+         </div>
 
        <div className="result">
        {list?.map((current) => <div>
@@ -52,7 +101,11 @@ function App() {
 
 
     </div>
+
+:
+<span> Loading... </span>
   );
+ 
 }
 
 export default App;
